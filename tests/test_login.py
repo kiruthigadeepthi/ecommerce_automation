778dd02a509ee8282f6b7057a7f9189642ea21c9
Kiruthiga <kiruthigadeepthi@gmail.com>
2026-02-20 15:22:55 +0530

    Login page added

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,9 @@
 from pages.login_page import LoginPage
-
+#Loginpage
 def test_valid_login(driver):
     login_page = LoginPage(driver)
     login_page.load()
     login_page.login("standard_user", "secret_sauce")
     assert "inventory.html" in driver.current_url
     #Updated login page
+    
